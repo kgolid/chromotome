1bf685a74f4bac0c2b4622da04415a92b304f957
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-01-03 21:00:39 +0100

    add new palettes

--- palettes.js
+++ palettes.js
@@ -103,6 +103,66 @@ const palettes = [
       '#ab2562',
       '#ca2847'
     ]
+  },
+  {
+    name: 'frozen-rose',
+    colors: ['#29368f', '#e9697b', '#1b164d', '#f7d996'],
+    background: '#f2e8e4'
+  },
+  {
+    name: 'winter-night',
+    colors: ['#122438', '#dd672e', '#87c7ca', '#ebebeb'],
+    background: '#ebebeb'
+  },
+  {
+    name: 'saami',
+    colors: ['#eab700', '#e64818', '#2c6393', '#eecfca'],
+    background: '#e7e6e4'
+  },
+  {
+    name: 'knotberry1',
+    colors: ['#20342a', '#f74713', '#686d2c', '#e9b4a6'],
+    background: '#e5ded8'
+  },
+  {
+    name: 'knotberry2',
+    colors: ['#1d3b1a', '#eb4b11', '#e5bc00', '#f29881'],
+    background: '#eae2d0'
+  },
+  {
+    name: 'tricolor',
+    colors: ['#ec643b', '#56b7ab', '#f8cb57', '#1f1e43'],
+    background: '#f7f2df'
+  },
+  {
+    name: 'rag-mysore',
+    colors: ['#ec6c26', '#613a53', '#e8ac52', '#639aa0'],
+    background: '#d5cda1'
+  },
+  {
+    name: 'rag-gol',
+    colors: ['#d3693e', '#803528', '#f1b156', '#90a798'],
+    background: '#f0e0a4'
+  },
+  {
+    name: 'rag-belur',
+    colors: ['#f46e26', '#68485f', '#3d273a', '#535d55'],
+    background: '#dcd4a6'
+  },
+  {
+    name: 'rag-bangalore',
+    colors: ['#ea720e', '#ca5130', '#e9c25a', '#52534f'],
+    background: '#f9ecd3'
+  },
+  {
+    name: 'rag-taj',
+    colors: ['#ce565e', '#8e1752', '#f8a100', '#3ac1a6'],
+    background: '#efdea2'
+  },
+  {
+    name: 'rag-virupaksha',
+    colors: ['#f5736a', '#925951', '#feba4c', '#9d9b9d'],
+    background: '#eedfa2'
   }
 ];
 
