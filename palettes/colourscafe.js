b5dc46d03aede1b8e9032d5e8b3da6a456535908
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-03-02 16:40:18 +0100

    update backgrounds and add a palette

--- palettes/colourscafe.js
+++ palettes/colourscafe.js
@@ -1,26 +1,37 @@
 export default [
   {
     name: 'cc239',
-    colors: ['#e3dd34', '#78496b', '#f0527f', '#e0eff0', '#a7e0e2']
+    colors: ['#e3dd34', '#78496b', '#f0527f', '#a7e0e2'],
+    background: '#e0eff0'
   },
   {
     name: 'cc234',
-    colors: ['#ffce49', '#ede8dc', '#f7f4ed', '#ff5736', '#ff99b4']
+    colors: ['#ffce49', '#ede8dc', '#ff5736', '#ff99b4'],
+    background: '#f7f4ed'
   },
   {
     name: 'cc232',
-    colors: ['#5c5f46', '#ff7044', '#ffce39', '#e9ecde', '#66aeaa']
+    colors: ['#5c5f46', '#ff7044', '#ffce39', '#66aeaa'],
+    background: '#e9ecde'
   },
   {
     name: 'cc238',
-    colors: ['#553c60', '#f5e9de', '#ffb0a0', '#ff6749', '#fbe090']
+    colors: ['#553c60', '#ffb0a0', '#ff6749', '#fbe090'],
+    background: '#f5e9de'
   },
   {
     name: 'cc242',
-    colors: ['#bbd444', '#faf4e4', '#fcd744', '#fa7b53', '#423c6f']
+    colors: ['#bbd444', '#fcd744', '#fa7b53', '#423c6f'],
+    background: '#faf4e4'
   },
   {
-    name: '245',
-    colors: ['#0d4a4e', '#ff947b', '#f6f4ed', '#ead3a2', '#5284ab']
+    name: 'cc245',
+    colors: ['#0d4a4e', '#ff947b', '#ead3a2', '#5284ab'],
+    background: '#f6f4ed'
+  },
+  {
+    name: 'cc273',
+    colors: ['#363d4a', '#7b8a56', '#ff9369', '#f4c172'],
+    background: '#f0efe2'
   }
 ];
