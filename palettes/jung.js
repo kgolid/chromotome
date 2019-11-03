c70665492925a23f22349f01e18bf9ecf404aa44
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-11-03 19:54:13 +0100

    Create jung.js

---
+++ palettes/jung.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'jung_bird',
+    colors: ['#fc3032', '#fed530', '#33c3fb', '#ff7bac', '#fda929'],
+    stroke: '#000000',
+    background: '#ffffff'
+  },
+  {
+    name: 'jung_horse',
+    colors: ['#e72e81', '#f0bf36', '#3056a2'],
+    stroke: '#000000',
+    background: '#ffffff'
+  },
+  {
+    name: 'jung_croc',
+    colors: ['#f13274', '#eed03e', '#405e7f', '#19a198'],
+    stroke: '#000000',
+    background: '#ffffff'
+  },
+  {
+    name: 'jung_hippo',
+    colors: ['#ff7bac', '#ff921e', '#3ea8f5', '#7ac943'],
+    stroke: '#000000',
+    background: '#ffffff'
+  },
+  {
+    name: 'jung_wolf',
+    colors: ['#e51c39', '#f1b844', '#36c4b7', '#666666'],
+    stroke: '#000000',
+    background: '#ffffff'
+  }
+];
