33f3a27c62f30660a38cacde3f40f9a0d2ebc821
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-11-03 14:46:41 +0100

    Create bekk.js

---
+++ palettes/bekk.js
@@ -0,0 +1,178 @@
+export default [
+  {
+    name: 'bekk00b',
+    colors: [
+      '#FFB88D',
+      '#BCCEDD',
+      '#B1E8FF',
+      '#E7E7E7',
+      '#FF9999',
+      '#FFF2AD',
+      '#E5B1FF',
+      '#A1F5E3',
+      '#6D7ABB'
+    ],
+    background: '#0E0E0E',
+    stroke: '#FFFFFF'
+  },
+  {
+    name: 'bekk00',
+    colors: [
+      '#FF8034',
+      '#7E9CB9',
+      '#43CBFF',
+      '#CECECE',
+      '#FF5B5B',
+      '#FFF02B',
+      '#8E24C9',
+      '#16DBC4',
+      '#162365'
+    ],
+    background: '#0E0E0E',
+    stroke: '#FFFFFF'
+  },
+  {
+    name: 'bekk01',
+    colors: ['#162365', '#FF5B5B', '#16DBC4'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk02',
+    colors: ['#FF8034', '#162365', '#16DBC4'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk03',
+    colors: ['#FFF02B', '#FF5B5B', '#16DBC4'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk04',
+    colors: ['#43CBFF', '#FFF02B', '#FF8034'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk05',
+    colors: ['#8E24C9', '#162365', '#FF8034'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk06',
+    colors: ['#FF5B5B', '#7E9CB9', '#8E24C9'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk07',
+    colors: ['#43CBFF', '#8E24C9', '#162365'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk08',
+    colors: ['#FFF02B', '#FF8034', '#162365'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk09',
+    colors: ['#7E9CB9', '#43CBFF', '#16DBC4'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk10',
+    colors: ['#162365', '#FF5B5B', '#FFF02B'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk11',
+    colors: ['#FFF02B', '#43CBFF', '#FF5B5B'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk12',
+    colors: ['#16DBC4', '#FFF02B', '#162365'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk01b',
+    colors: ['#6D7ABB', '#FF9999', '#A1F5E3'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk02b',
+    colors: ['#FFB88D', '#6D7ABB', '#A1F5E3'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk03b',
+    colors: ['#FFF2AD', '#FF9999', '#A1F5E3'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk04b',
+    colors: ['#B1E8FF', '#FFF2AD', '#FFB88D'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk05b',
+    colors: ['#E5B1FF', '#6D7ABB', '#FFB88D'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk06b',
+    colors: ['#FF9999', '#BCCEDD', '#E5B1FF'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk07b',
+    colors: ['#B1E8FF', '#E5B1FF', '#6D7ABB'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk08b',
+    colors: ['#FFF2AD', '#FFB88D', '#6D7ABB'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk09b',
+    colors: ['#BCCEDD', '#B1E8FF', '#A1F5E3'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk10b',
+    colors: ['#6D7ABB', '#FF9999', '#FFF2AD'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk11b',
+    colors: ['#FFF2AD', '#B1E8FF', '#FF9999'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  },
+  {
+    name: 'bekk12b',
+    colors: ['#A1F5E3', '#FFF2AD', '#6D7ABB'],
+    stroke: '#0E0E0E',
+    background: '#FFFFFF'
+  }
+];
