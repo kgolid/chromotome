425fa72d7fa8b1db25cb1ecf204d444ce78942a8
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-06 01:03:30 +0100

    add system sketches

--- palettes/system.js
+++ palettes/system.js
@@ -13,14 +13,38 @@ export default [
   },
   {
     name: 'system.#03',
-    colors: ['#f25263', '#e6af55', '#058f9b', '#d7785d', '#0168b7', '#938475', '#f0f0ee'],
+    colors: ['#f73f4a', '#d3e5eb', '#002c3e', '#1aa1b1', '#ec6675'],
     stroke: '#110b09',
-    background: '#e9d5bd'
+    background: '#fff'
   },
   {
     name: 'system.#04',
     colors: ['#e31f4f', '#f0ac3f', '#18acab', '#26265a', '#ea7d81', '#dcd9d0'],
     stroke: '#26265a',
     backgrund: '#dcd9d0'
+  },
+  {
+    name: 'system.#05',
+    colors: ['#db4549', '#d1e1e1', '#3e6a90', '#2e3853', '#a3c9d3'],
+    stroke: '#000',
+    background: '#fff'
+  },
+  {
+    name: 'system.#06',
+    colors: ['#e5475c', '#95b394', '#28343b', '#f7c6a3', '#eb8078'],
+    stroke: '#000',
+    background: '#fff'
+  },
+  {
+    name: 'system.#07',
+    colors: ['#d75c49', '#f0efea', '#509da4'],
+    stroke: '#000',
+    background: '#fff'
+  },
+  {
+    name: 'system.#08',
+    colors: ['#f6625a', '#92b29f', '#272c3f'],
+    stroke: '#000',
+    background: '#fff'
   }
 ];
