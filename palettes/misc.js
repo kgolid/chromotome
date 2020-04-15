475a88f20dcce51ab3bc6d5c048d45bd352f48b8
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-04-16 00:03:46 +0200

    trailing commas after prettier update

--- palettes/misc.js
+++ palettes/misc.js
@@ -2,139 +2,137 @@ export default [
   {
     name: 'frozen-rose',
     colors: ['#29368f', '#e9697b', '#1b164d', '#f7d996'],
-    background: '#f2e8e4'
+    background: '#f2e8e4',
   },
   {
     name: 'winter-night',
     colors: ['#122438', '#dd672e', '#87c7ca', '#ebebeb'],
-    background: '#ebebeb'
+    background: '#ebebeb',
   },
   {
     name: 'saami',
     colors: ['#eab700', '#e64818', '#2c6393', '#eecfca'],
-    background: '#e7e6e4'
+    background: '#e7e6e4',
   },
   {
     name: 'knotberry1',
     colors: ['#20342a', '#f74713', '#686d2c', '#e9b4a6'],
-    background: '#e5ded8'
+    background: '#e5ded8',
   },
   {
     name: 'knotberry2',
     colors: ['#1d3b1a', '#eb4b11', '#e5bc00', '#f29881'],
-    background: '#eae2d0'
+    background: '#eae2d0',
   },
   {
     name: 'tricolor',
     colors: ['#ec643b', '#56b7ab', '#f8cb57', '#1f1e43'],
-    background: '#f7f2df'
+    background: '#f7f2df',
   },
   {
     name: 'foxshelter',
     colors: ['#ff3931', '#007861', '#311f27', '#bab9a4'],
-    background: '#dddddd'
+    background: '#dddddd',
   },
   {
     name: 'hermes',
     colors: ['#253852', '#51222f', '#b53435', '#ecbb51'],
-    background: '#eeccc2'
+    background: '#eeccc2',
   },
   {
     name: 'olympia',
     colors: ['#ff3250', '#ffb33a', '#008c36', '#0085c6', '#4c4c4c'],
     stroke: '#0b0b0b',
-    background: '#faf2e5'
+    background: '#faf2e5',
   },
   {
     name: 'byrnes',
     colors: ['#c54514', '#dca215', '#23507f'],
     stroke: '#0b0b0b',
-    background: '#e8e7d4'
+    background: '#e8e7d4',
   },
   {
     name: 'butterfly',
     colors: ['#f40104', '#f6c0b3', '#99673a', '#f0f1f4'],
     stroke: '#191e36',
-    background: '#191e36'
+    background: '#191e36',
   },
   {
     name: 'floratopia',
     colors: ['#bf4a2b', '#cd902a', '#4e4973', '#f5d4bc'],
     stroke: '#1e1a43',
-    background: '#1e1a43'
+    background: '#1e1a43',
   },
   {
     name: 'verena',
     colors: ['#f1594a', '#f5b50e', '#14a160', '#2969de', '#885fa4'],
     stroke: '#1a1a1a',
-    background: '#e2e6e8'
   },
   {
     name: 'empusa',
     colors: ['#c92a28', '#e69301', '#1f8793', '#13652b', '#e7d8b0', '#48233b', '#e3b3ac'],
     stroke: '#1a1a1a',
     background: '#f0f0f2'
+    background: '#e2e6e8',
   },
   {
     name: 'florida_citrus',
     colors: ['#ea7251', '#ebf7f0', '#02aca5'],
     stroke: '#050100',
-    background: '#9ae2d3'
+    background: '#9ae2d3',
   },
   {
     name: 'lemon_citrus',
     colors: ['#e2d574', '#f1f4f7', '#69c5ab'],
     stroke: '#463231',
-    background: '#f79eac'
+    background: '#f79eac',
   },
   {
     name: 'yuma_punk',
     colors: ['#f05e3b', '#ebdec4', '#ffdb00'],
     stroke: '#ebdec4',
-    background: '#161616'
+    background: '#161616',
   },
   {
     name: 'moir',
     colors: ['#a49f4f', '#d4501e', '#f7c558', '#ebbaa6'],
     stroke: '#161716',
-    background: '#f7f4ef'
+    background: '#f7f4ef',
   },
   {
     name: 'sprague',
     colors: ['#ec2f28', '#f8cd28', '#1e95bb', '#fbaab3', '#fcefdf'],
     stroke: '#221e1f',
-    background: '#fcefdf'
+    background: '#fcefdf',
   },
   {
     name: 'bloomberg',
     colors: ['#ff5500', '#f4c145', '#144714', '#2f04fc', '#e276af'],
     stroke: '#000',
-    background: '#fff3dd'
+    background: '#fff3dd',
   },
   {
     name: 'revolucion',
     colors: ['#ed555d', '#fffcc9', '#41b797', '#eda126', '#7b5770'],
     stroke: '#fffcc9',
-    background: '#2d1922'
+    background: '#2d1922',
   },
   {
     name: 'sneaker',
     colors: ['#e8165b', '#401e38', '#66c3b4', '#ee7724', '#584098'],
     stroke: '#401e38',
-    background: '#ffffff'
+    background: '#ffffff',
   },
   {
     name: 'miradors',
     colors: ['#ff6936', '#fddc3f', '#0075ca', '#00bb70'],
     stroke: '#ffffff',
-    background: '#020202'
+    background: '#020202',
   },
   {
     name: 'kaffeprat',
     colors: ['#BCAA8C', '#D8CDBE', '#484A42', '#746B58', '#9A8C73'],
     stroke: '#000',
-    background: '#fff'
-  },
   {
     name: 'delphi',
     colors: ['#475b62', '#7a999c', '#2a1f1d', '#fbaf3c', '#df4a33', '#f0e0c6', '#af592c'],
@@ -162,4 +160,6 @@ export default [
     stroke: '#180305',
     background: '#ede4cb'
   }
+    background: '#fff',
+  },
 ];
