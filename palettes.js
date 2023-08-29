ac55de0f4ed49ddea7ba5c5e80625e110cf65204
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2023-08-30 00:03:09 +0200

    add missing file

--- palettes.js
+++ palettes.js
@@ -1,24 +1,25 @@
-import misc from './palettes/misc';
-import colourscafe from './palettes/colourscafe';
-import ranganath from './palettes/ranganath';
-import roygbivs from './palettes/roygbivs';
-import tundra from './palettes/tundra';
-import rohlfs from './palettes/rohlfs';
-import ducci from './palettes/ducci';
-import judson from './palettes/judson';
-import iivonen from './palettes/iivonen';
-import kovecses from './palettes/kovecses';
-import tsuchimochi from './palettes/tsuchimochi';
-import duotone from './palettes/duotone';
-import hilda from './palettes/hilda';
-import spatial from './palettes/spatial';
-import jung from './palettes/jung';
-import system from './palettes/system';
-import flourish from './palettes/flourish';
-import dale from './palettes/dale';
-import cako from './palettes/cako';
-import mayo from './palettes/mayo';
-import exposito from './palettes/exposito';
+import misc from "./palettes/misc";
+import colourscafe from "./palettes/colourscafe";
+import ranganath from "./palettes/ranganath";
+import roygbivs from "./palettes/roygbivs";
+import tundra from "./palettes/tundra";
+import rohlfs from "./palettes/rohlfs";
+import ducci from "./palettes/ducci";
+import judson from "./palettes/judson";
+import iivonen from "./palettes/iivonen";
+import kovecses from "./palettes/kovecses";
+import tsuchimochi from "./palettes/tsuchimochi";
+import duotone from "./palettes/duotone";
+import hilda from "./palettes/hilda";
+import spatial from "./palettes/spatial";
+import jung from "./palettes/jung";
+import system from "./palettes/system";
+import flourish from "./palettes/flourish";
+import dale from "./palettes/dale";
+import cako from "./palettes/cako";
+import mayo from "./palettes/mayo";
+import exposito from "./palettes/exposito";
+import orbifold from "./palettes/orbifold";
 
 const pals = misc.concat(
   ranganath,
@@ -40,10 +41,11 @@ const pals = misc.concat(
   dale,
   cako,
   mayo,
-  exposito
+  exposito,
+  orbifold
 );
 
-export default pals.map((p) => {
+export default pals.map(p => {
   p.size = p.colors.length;
   return p;
 });
